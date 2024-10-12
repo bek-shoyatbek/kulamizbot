fafb98da2f66859ac787eadc051997989e29e3c0
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-12 20:24:26 +0500

    feat: dist file removed

--- dist/models/voice.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-const VoiceSchema = new Schema({
-    userId: { type: Number, required: true },
-    title: { type: String, required: true },
-    fileId: { type: String, required: true },
-    duration: { type: Number, required: true },
-});
-export const Voice = mongoose.model("Voice", VoiceSchema);
--- dist/shared/configs/index.js
+++
@@ -1,4 +0,0 @@
-import "dotenv/config";
-export const BOT_TOKEN = process.env.BOT_TOKEN;
-export const MONGODB_URI = process.env.MONGODB_URI;
-export const APP_PORT = process.env.APP_PORT;
